8f452902ff431fdaf55af882374f8cfe3a0bd18f
vishnu vs <54799446+vishnuvs369@users.noreply.github.com>
2021-04-24 21:27:49 +0530

    added deck

--- main.go
+++ main.go
@@ -1,16 +1,12 @@
 package main
 
-import "fmt"
-
 func main() {
-	cards := []string{"Ace of Diamonds", newCard()}
+	cards := deck{"Ace of Diamonds", newCard()}
 	cards = append(cards, "six of spades")
 
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
+	cards.print()
 }
 
 func newCard() string {
-	return "Five of Diamonds"
+	return "Five of diamonds"
 }
